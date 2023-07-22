3969864928b8bfe851e499211e8b1b7d5b69a6b4
Paul Schofield <paul.schofield.us@gmail.com>
2023-07-22 14:50:35 -0400

    update db name and write integer-check test case

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,20 @@
-import json
 import boto3
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('http-crud-tutorial-items')
-def handler(event, context):
-    response = table.get_item(Key={
-        'id':'0'
-    })
-    views = response['Item']['views']
-    views = views + 1
-    print(views)
-    response = table.put_item(Item={
-            'id':'0',
-            'views': views
-    })
+from decimal import Decimal
 
-    return views
+def handler(event, context, dynamodb_resource=None):
+    if not dynamodb_resource:
+        dynamodb_resource = boto3.resource('dynamodb')
+
+    table = dynamodb_resource.Table('view-count')  # Replace 'your_table_name' with the actual table name
+    response = table.get_item(Key={'id': '0'})
+
+    item = response.get('Item', {})
+    views = item.get('views', 0)
+    new_views = views + 1
+
+    # Convert the new_views to an integer before putting it in the table
+    new_views = int(new_views)
+
+    table.put_item(Item={'id': '0', 'views': new_views})
+
+    return new_views
---
+++ tests/test_lambda_function.py
@@ -0,0 +1,36 @@
+import pytest
+from unittest.mock import MagicMock
+
+import os
+import sys
+
+# Add root directory to Python path
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, root_dir)
+
+# Import handler function
+from lambda_function import handler
+
+@pytest.fixture
+def mock_dynamodb_resource():
+    return MagicMock()
+
+def test_handler_with_mock_dynamodb_resource(mock_dynamodb_resource):
+    # Set up the mock response for get_item
+    mock_dynamodb_resource.Table.return_value.get_item.return_value = {
+        'Item': {
+            'id': '0',
+            'views': 5
+        }
+    }
+
+    # Invoke the handler with a sample event
+    event = {
+        'key1': 'value1',
+        'key2': 'value2'
+    }
+    context = None
+    result = handler(event, context, dynamodb_resource=mock_dynamodb_resource)
+
+    # Assert that the result is an integer
+    assert isinstance(result, int)
